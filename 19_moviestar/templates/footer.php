186e282d71a46ab0ee12345518e820337de712ff
Raul Lourenço <raul_job@hotmail.com>
2022-06-06 19:13:01 -0300

    19_moviestar

---
+++ 19_moviestar/templates/footer.php
@@ -0,0 +1,29 @@
+<footer id="footer">
+    <div id="social-container">
+        <ul>
+            <li>
+                <a href="#"><i class="fab fa-facebook-square"></i></a>
+            </li>
+            <li>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+            </li>
+            <li>
+                <a href="#"><i class="fab fa-youtube"></i></a>
+            </li>
+        </ul>
+    </div>
+    <div id="footer-links-container">
+        <ul>
+            <li><a href="#">Adicionar filme</a></li>
+            <li><a href="#">Adicionar crítica</a></li>
+            <li><a href="#">Entrar / Registrar</a></li>
+        </ul>
+    </div>
+    <p>&copy; 2020 Hora de Codar</p>
+  </footer>
+    <!-- Bootstrap JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0-beta1/js/bootstrap.js" integrity="sha512-kZzIlRPOhobf+l/HSLHKu5E6UVe34lDZKtQzNEMHRNdctaVhwIPVruz1ahHqtMUTuYxJaSmVIZyWjV7jlfpAyw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- JQUERY -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+</html>
