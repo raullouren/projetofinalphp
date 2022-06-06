017f49a37b917a3d8d32a9455759b2419f90b04d
Raul Lourenco <102099651+raullouren@users.noreply.github.com>
2022-06-06 20:05:42 -0300

    Delete txtbd directory

--- txtbd/index.php
+++
@@ -1,7 +0,0 @@
-  <form name="dbase" method="post" action="cadastrar.php">
-   2:      Usuário: <input type="text" name="user">
-   3:      Email: <input type="text" name="email">
-   4:      Senha: <input type="text" name="senha">
-   5:      Confirmar senha: <input type="text" name="csenha">
-   6:      <input type="submit" value="Enviar dados">
-   7:  </form>
